3b55d8f2ee0ce90735b2ccfa066b90f1ed71e3ce
Johnny Svensson <johnny.svensson15@gmail.com>
2025-07-19 09:13:18 +0200

    Cant find powerlevel, adding as extra config

--- homemanager/zsh/zsh.nix
+++ homemanager/zsh/zsh.nix
@@ -1,16 +1,15 @@
 {inputs, lib, pkgs, config, ...} : {
-
-  
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
     oh-my-zsh = {
       enable = true;
-      theme = "powerlevel10k/powerlevel10k";
       plugins = [ "git" "z" "sudo" ];
+      extraConfig = ''
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      '';
     };
     shellAliases = {
       ls = "ls -lah --color";
@@ -18,9 +17,33 @@
       nixb = "sudo nixos-rebuild switch --flake #johnny-home --show-trace";
     };
   };
-
-  home.packages = with pkgs; [
-    zsh-powerlevel10k
-  ];
 }
+
+
+
+#{inputs, lib, pkgs, config, ...} : {
+#
+#  
+#  programs.zsh = {
+#    enable = true;
+#    enableCompletion = true;
+#    autosuggestion.enable = true;
+#    syntaxHighlighting.enable = true;
+#
+#    oh-my-zsh = {
+#      enable = true;
+#      theme = "powerlevel10k/powerlevel10k";
+#      plugins = [ "git" "z" "sudo" ];
+#    };
+#    shellAliases = {
+#      ls = "ls -lah --color";
+#      gs = "git status";
+#      nixb = "sudo nixos-rebuild switch --flake #johnny-home --show-trace";
+#    };
+#  };
+#
+#  home.packages = with pkgs; [
+#    zsh-powerlevel10k
+#  ];
+#}
 
