d5cfc5762b7776408b7a8fbd9de473bf20d16138
Johnny Svensson <johnny.svensson15@gmail.com>
2025-09-08 12:42:52 +0200

    update monday

--- homemanager/yazi/yazi.nix
+++ homemanager/yazi/yazi.nix
@@ -3,8 +3,20 @@
 
   programs.yazi = {
     enable = true;
+
+    extraConfig = ''
+      [mgr]
+      prepend_keymap = [
+        # Yank a single file and copy its path to the system clipboard
+        { on = "yy", run = "echo {file} | wl-copy", desc = "Yank file path to system clipboard" }
+
+        # Yank multiple selected files
+        { on = "Y", run = "echo {selection} | wl-copy", desc = "Yank selected files to system clipboard" }
+      ]
+    '';
   };
 
+  
   xdg.desktopEntries.yazi = {
     name = "Yazi Terminal File Manager";  # More descriptive name
     comment = "Terminal file manager";
