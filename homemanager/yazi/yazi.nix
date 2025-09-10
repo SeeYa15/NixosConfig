3ee966dee3c6f4791f540e817afa4d9b1efb87a9
Johnny Svensson <johnny.svensson15@gmail.com>
2025-09-10 07:57:06 +0200

    update wednsday

--- homemanager/yazi/yazi.nix
+++ homemanager/yazi/yazi.nix
@@ -3,17 +3,6 @@
 
   programs.yazi = {
     enable = true;
-
-    extraConfig = ''
-      [mgr]
-      prepend_keymap = [
-        # Yank a single file and copy its path to the system clipboard
-        { on = "yy", run = "echo {file} | wl-copy", desc = "Yank file path to system clipboard" }
-
-        # Yank multiple selected files
-        { on = "Y", run = "echo {selection} | wl-copy", desc = "Yank selected files to system clipboard" }
-      ]
-    '';
   };
 
   
