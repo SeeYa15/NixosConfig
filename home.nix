5dfdacf7897c3fe6f0844a79194036e10e7f2528
Johnny Svensson <johnny.svensson15@gmail.com>
2025-10-15 12:45:37 +0200

    update wednesday

--- home.nix
+++ home.nix
@@ -48,12 +48,12 @@
     obs-studio
     #Develop Packages
     # ...
-     (python3.withPackages (python-pkgs: with python-pkgs; [
-       pandas
-       requests
-       tkinter
-       pillow
-     ]))
+     # (python3.withPackages (python-pkgs: with python-pkgs; [
+     #   pandas
+     #   requests
+     #   tkinter
+     #   pillow
+     # ]))
 
     #C++ packages
     gcc
