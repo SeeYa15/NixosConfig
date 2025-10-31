e3383c9af620bfae54dff155ef80dc5c6a73093c
Johnny Svensson <johnny.svensson15@gmail.com>
2025-10-31 13:06:18 +0100

    update friday

--- homemanager/yazi/yazi.nix
+++ homemanager/yazi/yazi.nix
@@ -3,10 +3,12 @@
 
   programs.yazi = {
     enable = true;
-    mgr = {
-      show_hidden = true;
-      sort_by = "mtime";
-      sort_dir_first = true;
+    settings = {
+      mgr = {
+        show_hidden = true;
+        sort_by = "mtime";
+        sort_dir_first = true;
+      };
     };
   };
 
