055b8bfb423f67954215a855623f8257563aa9a8
Johnny Svensson <johnny.svensson15@gmail.com>
2025-11-11 11:29:09 +0100

    lua update scripts

--- homemanager/neovim/luascripts/Buffermovement.lua
+++ homemanager/neovim/luascripts/Buffermovement.lua
@@ -1,6 +1,6 @@
 
 vim.api.nvim_create_user_command("BufMovement", function ()
-  vim.cmd(bnext)
+  vim.cmd("bnext")
 end, {})
 
 vim.keymap.set("n", "<TAB>", "<cmd>BufMovement<cr>", {desc = "Test tabbing"})
