4dea9002e43a00fde8074de4fc5a85f16cc6f76c
Johnny Svensson <johnny.svensson15@gmail.com>
2026-02-06 11:28:33 +0100

    added vs profile

--- Shared/homemanager/vscode/vscode.nix
+++ Shared/homemanager/vscode/vscode.nix
@@ -2,19 +2,19 @@
   programs.vscode = {
     enable = true;
 
-    # profiles = {
-    #   default = {
-    #     extensions = with pkgs.nix-vscode-extensions.vscode-marketplace; [
-    #       ms-dotnettools.csdevkit
-    #       ms-dotnettools.csharp
-    #       ms-vscode-remote.remote-containers
-    #       ms-vscode.theme-tomorrowkit
-    #       dbaeumer.vscode-eslint
-    #       esbenp.prettier-vscode
-    #       patcx.vscode-nuget-gallery
-    #       github.codespaces
-    #     ];
-    #   };
-    # };
+    profiles = {
+      default = {
+        extensions = with pkgs.nix-vscode-extensions.vscode-marketplace; [
+          ms-dotnettools.csdevkit
+          ms-dotnettools.csharp
+          ms-vscode-remote.remote-containers
+          ms-vscode.theme-tomorrowkit
+          dbaeumer.vscode-eslint
+          esbenp.prettier-vscode
+          patcx.vscode-nuget-gallery
+          github.codespaces
+        ];
+      };
+    };
   };
 }
