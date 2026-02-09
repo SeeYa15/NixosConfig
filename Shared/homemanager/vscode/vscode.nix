41460aa99b98faa9b7f688d01f8af7f4181973cd
Johnny Svensson <johnny.svensson15@gmail.com>
2026-02-09 21:09:10 +0100

    stuff

--- Shared/homemanager/vscode/vscode.nix
+++ Shared/homemanager/vscode/vscode.nix
@@ -7,18 +7,18 @@ in
   programs.vscode = {
     enable = true;
     
-    profiles = {
-      default = {
-        extensions = with extensions.vscode-marketplace; [
-          ms-dotnettools.csdevkit
-          ms-dotnettools.csharp
-          ms-vscode-remote.remote-containers
-          ms-vscode.theme-tomorrowkit
-          dbaeumer.vscode-eslint
-          esbenp.prettier-vscode
-          patcx.vscode-nuget-gallery
-          github.codespaces
-        ];
+    # profiles = {
+    #   default = {
+    #     extensions = with extensions.vscode-marketplace; [
+    #       ms-dotnettools.csdevkit
+    #       ms-dotnettools.csharp
+    #       ms-vscode-remote.remote-containers
+    #       ms-vscode.theme-tomorrowkit
+    #       dbaeumer.vscode-eslint
+    #       esbenp.prettier-vscode
+    #       patcx.vscode-nuget-gallery
+    #       github.codespaces
+    #     ];
     # extensions = with pkgs.vscode-extensions; [
     #   ms-dotnettools.csdevkit
     #   ms-dotnettools.csharp
