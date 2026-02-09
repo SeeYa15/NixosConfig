a0455fa5e62e42441395f01950dc16c7b9446579
Johnny Svensson <johnny.svensson15@gmail.com>
2026-02-09 10:50:56 +0100

    stuff

--- Shared/homemanager/vscode/vscode.nix
+++ Shared/homemanager/vscode/vscode.nix
@@ -3,7 +3,7 @@
     enable = true;
     profiles = {
       default = {
-        extensions = inputs.nix-vscode-extensions.vscode-marketplace; [
+        extensions = with inputs.nix-vscode-extensions.vscode-marketplace; [
           ms-dotnettools.csdevkit
           ms-dotnettools.csharp
           ms-vscode-remote.remote-containers
