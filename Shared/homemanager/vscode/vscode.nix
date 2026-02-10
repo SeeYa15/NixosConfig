549191eae5d0591eff965598fd5ba1caba5149ed
Johnny Svensson <johnny.svensson15@gmail.com>
2026-02-10 07:29:45 +0100

    VS CODE EXTENSION SHITS DOESNT WORK

--- Shared/homemanager/vscode/vscode.nix
+++ Shared/homemanager/vscode/vscode.nix
@@ -6,19 +6,19 @@ in
   
   programs.vscode = {
     enable = true;
-    profiles = {
-      default = {
-        extensions = with extensions.vscode-marketplace; [
-          ms-dotnettools.csdevkit
-          ms-dotnettools.csharp
-          ms-vscode-remote.remote-containers
-          ms-vscode.theme-tomorrowkit
-          dbaeumer.vscode-eslint
-          esbenp.prettier-vscode
-          patcx.vscode-nuget-gallery
-          github.codespaces
-        ];
-      };
-    };
+    # profiles = {
+    #   default = {
+    #     extensions = with extensions.vscode-marketplace; [
+    #       ms-dotnettools.csdevkit
+    #       ms-dotnettools.csharp
+    #       ms-vscode-remote.remote-containers
+    #       ms-vscode.theme-tomorrowkit
+    #       dbaeumer.vscode-eslint
+    #       esbenp.prettier-vscode
+    #       patcx.vscode-nuget-gallery
+    #       github.codespaces
+    #     ];
+    #   };
+    # };
   };
 }
