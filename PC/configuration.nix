3704636a3453ca1156a35a0abcf16ede53112e8d
Johnny Svensson <johnny.svensson15@gmail.com>
2026-05-15 09:10:58 +0200

    Cannot build bottles cause of openldap test error

--- PC/configuration.nix
+++ PC/configuration.nix
@@ -203,9 +203,26 @@
     devcontainer
     docker
     github-copilot-cli
-    bottles
+    (bottles.override {
+      # Intercept buildFHSEnv to modify target packages
+      buildFHSEnv = args: pkgs.buildFHSEnv (args // {
+        multiPkgs = envPkgs:
+          let
+            # Fetch original package list
+            originalPkgs = args.multiPkgs envPkgs;
+
+            # Disable tests for openldap
+            customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
+          in
+          # Replace broken openldap with the custom one
+          builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+      });
+    })
     wine
+    eww
   ];
+
+  
   environment.sessionVariables = {
     #DOTNET_ROOT = "${dotnetCombined}/share/dotnet";
     LD_LIBRARY_PATH = "/run/opengl-driver/lib:$LD_LIBRARY_PATH";
